fdd86ecc7aaf1c7cdb9db75a165ff446cddbe062
Justin Zhu <dr_kimsora@yahoo.com>
2019-07-29 14:59:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var stream = require('stream');
 
 app.set('view engine', 'ejs');
 
-const WS_PORT = process.env.WS_PORT || 3001;
+const WS_PORT = process.env.PORT || 3001;
 
-const HTTP_PORT = process.env.HTTP_PORT || 3000;
+const HTTP_PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res){
 	res.render("test");
